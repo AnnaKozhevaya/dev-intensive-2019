60d365be557780e4a15d93c64bcfce3da45dab68
Anna Kozhevaya <kb120037@gmail.com>
2019-08-04 21:45:34 +0300

    hometask_4 + fix nick name

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -9,7 +9,7 @@ fun User.toUserView() : UserView {
 
     val nickName = Utils.transliteration("$firstName $lastName")
     val initials = Utils.toInitials(firstName, lastName)
-    val status = if (lastVisit == null) "Еще ни разу не быд" else if (isOnline) "online" else "Последний раз был ${Date().humanizeDiff()}"
+    val status = if (lastVisit == null) "Еще ни разу не был" else if (isOnline) "online" else "Последний раз был ${Date().humanizeDiff()}"
 
     return UserView(
         id,
